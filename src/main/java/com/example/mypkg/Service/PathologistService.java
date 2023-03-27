5c375754b86f60214b706a092ebbcbd6e53f4fa5
Mohibul Islam <104147741+mohibul361@users.noreply.github.com>
2023-03-28 02:28:05 +0600

    Update PathologistService.java

--- src/main/java/com/example/mypkg/Service/PathologistService.java
+++ src/main/java/com/example/mypkg/Service/PathologistService.java
@@ -1,18 +1,17 @@
-package com.example.mypkg.Service;
+package com.example.HospitalManagementSystem.Service;
 
-import com.example.mypkg.Model.Pathologist;
-import com.example.mypkg.Payload.Request.PathologistRequest;
-import com.example.mypkg.Payload.Response.MessageResponse;
+import com.example.HospitalManagementSystem.Model.Pathologist;
+import com.example.HospitalManagementSystem.Payload.Request.PathologistRequest;
+import com.example.HospitalManagementSystem.Payload.Response.MessageResponse;
 import org.springframework.stereotype.Component;
 
 import java.util.List;
 import java.util.Optional;
-
 @Component
 public interface PathologistService {
     MessageResponse createPathologist(PathologistRequest pathologistRequest);
-    Optional<Pathologist> updatePathologist(Integer pathologistId, PathologistRequest pathologistRequest);
-    void deletePathologist(Integer pathologistId);
+    MessageResponse updatePathologist(Integer pathologistId, PathologistRequest pathologistRequest);
+    MessageResponse deletePathologist(Integer pathologistId);
     Pathologist getASinglePathologist(Integer pathologistId);
     List<Pathologist> getAllPathologist();
 }
