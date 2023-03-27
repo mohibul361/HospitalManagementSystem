d993b749dc4153e7929c4f1eb427c961e32dbb6b
Mohibul Islam <104147741+mohibul361@users.noreply.github.com>
2023-03-28 02:31:22 +0600

    Update ReceptionistService.java

--- src/main/java/com/example/mypkg/Service/ReceptionistService.java
+++ src/main/java/com/example/mypkg/Service/ReceptionistService.java
@@ -1,8 +1,8 @@
-package com.example.mypkg.Service;
+package com.example.HospitalManagementSystem.Service;
 
-import com.example.mypkg.Model.Receptionist;
-import com.example.mypkg.Payload.Request.ReceptionistRequest;
-import com.example.mypkg.Payload.Response.MessageResponse;
+import com.example.HospitalManagementSystem.Model.Receptionist;
+import com.example.HospitalManagementSystem.Payload.Request.ReceptionistRequest;
+import com.example.HospitalManagementSystem.Payload.Response.MessageResponse;
 import org.springframework.stereotype.Component;
 
 import java.util.List;
@@ -11,9 +11,8 @@ import java.util.Optional;
 @Component
 public interface ReceptionistService {
     MessageResponse createReceptionist(ReceptionistRequest receptionistRequest);
-    Optional<Receptionist> updateReceptionist(Integer receptionistId, ReceptionistRequest receptionistRequest);
-    void deleteReceptionist(Integer receptionistId);
+    MessageResponse updateReceptionist(Integer receptionistId, ReceptionistRequest receptionistRequest);
+    MessageResponse deleteReceptionist(Integer receptionistId);
     Receptionist getASingleReceptionist(Integer receptionistId);
     List<Receptionist> getAllReceptionist();
 }
-
