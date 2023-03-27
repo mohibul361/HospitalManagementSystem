ecd4b56507f15277515d40f7689de7fafa4de1d9
Mohibul Islam <104147741+mohibul361@users.noreply.github.com>
2023-03-28 02:28:46 +0600

    Update PathologistServiceImpl.java

--- src/main/java/com/example/mypkg/Service/PathologistServiceImpl.java
+++ src/main/java/com/example/mypkg/Service/PathologistServiceImpl.java
@@ -1,25 +1,23 @@
-package com.example.mypkg.Service;
+package com.example.HospitalManagementSystem.Service;
 
-import com.example.mypkg.Model.Pathologist;
-import com.example.mypkg.Payload.Request.PathologistRequest;
-import com.example.mypkg.Payload.Response.MessageResponse;
-import com.example.mypkg.Repository.PathologistRepository;
-import com.example.mypkg.Exception.ResourceNotFoundException;
+import com.example.HospitalManagementSystem.Exception.ResourceNotFoundException;
+import com.example.HospitalManagementSystem.Model.Pathologist;
+import com.example.HospitalManagementSystem.Payload.Request.PathologistRequest;
+import com.example.HospitalManagementSystem.Payload.Response.MessageResponse;
+import com.example.HospitalManagementSystem.Repository.PathologistRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Optional;
-
 @Service
-public class PathologistServiceImpl implements PathologistService {
-	
+public class PathologistServiceImpl implements PathologistService{
     @Autowired
     PathologistRepository pathologistRepository;
 
     @Override
     public MessageResponse createPathologist(PathologistRequest pathologistRequest) {
-    	Pathologist newPathologist = new Pathologist();
+        Pathologist newPathologist = new Pathologist();
         newPathologist.setFirstName(pathologistRequest.getFirstName());
         newPathologist.setLastname(pathologistRequest.getLastname());
         newPathologist.setPhoneNumber(pathologistRequest.getPhoneNumber());
@@ -32,20 +30,20 @@ public class PathologistServiceImpl implements PathologistService {
     }
 
     @Override
-    public Optional<Pathologist> updatePathologist(Integer pathologistId, PathologistRequest pathologistRequest)  throws ResourceNotFoundException{
+    public MessageResponse  updatePathologist(Integer pathologistId, PathologistRequest pathologistRequest)  throws ResourceNotFoundException {
         Optional<Pathologist> pathologist = pathologistRepository.findById(pathologistId);
         if (pathologist.isEmpty()){
-        throw new ResourceNotFoundException("Pathologist", "id", pathologistId);
+            throw new ResourceNotFoundException("Pathologist", "id", pathologistId);
         }
         else
-        pathologist.get().setFirstName(pathologistRequest.getFirstName());
+            pathologist.get().setFirstName(pathologistRequest.getFirstName());
         pathologist.get().setLastname(pathologistRequest.getLastname());
         pathologist.get().setPhoneNumber(pathologistRequest.getPhoneNumber());
         pathologist.get().setEmail(pathologistRequest.getEmail());
         pathologist.get().setDepartment(pathologistRequest.getDepartment());
         pathologist.get().setSalary(pathologistRequest.getSalary());
         pathologistRepository.save(pathologist.get());
-        return pathologist;
+        return new MessageResponse("Pathologist edited successfully");
     }
 
     @Override
@@ -58,9 +56,11 @@ public class PathologistServiceImpl implements PathologistService {
         return pathologistRepository.findAll();
     }
     @Override
-    public void deletePathologist(Integer pathologistId) throws ResourceNotFoundException {
+    public MessageResponse deletePathologist(Integer pathologistId) throws ResourceNotFoundException {
         if (pathologistRepository.getById(pathologistId).getId().equals(pathologistId)){
-        	pathologistRepository.deleteById(pathologistId);
+            pathologistRepository.deleteById(pathologistId);
+            return new MessageResponse("Pathologist deleted successfully");
+
         }
         else throw new ResourceNotFoundException("Pathologist", "id", pathologistId);
     }
