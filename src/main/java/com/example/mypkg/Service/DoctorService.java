ab46568e011bc89e99e19933eae071899962ba9c
Mohibul Islam <104147741+mohibul361@users.noreply.github.com>
2023-03-28 02:26:02 +0600

    Update DoctorService.java

--- src/main/java/com/example/mypkg/Service/DoctorService.java
+++ src/main/java/com/example/mypkg/Service/DoctorService.java
@@ -1,18 +1,18 @@
-package com.example.mypkg.Service;
+package com.example.HospitalManagementSystem.Service;
 
-import com.example.mypkg.Model.Doctor;
-import com.example.mypkg.Payload.Request.DoctorRequest;
-import com.example.mypkg.Payload.Response.MessageResponse;
+import com.example.HospitalManagementSystem.Model.Doctor;
+import com.example.HospitalManagementSystem.Payload.Request.DoctorRequest;
+import com.example.HospitalManagementSystem.Payload.Response.MessageResponse;
 import org.springframework.stereotype.Component;
 
 import java.util.List;
 import java.util.Optional;
-
 @Component
 public interface DoctorService {
     MessageResponse createDoctor(DoctorRequest doctorRequest);
-    Optional<Doctor> updateDoctor(Integer doctorId, DoctorRequest doctorRequest);
-    void deleteDoctor(Integer doctorId);
+    MessageResponse updateDoctor(Integer doctorId, DoctorRequest doctorRequest);
+    MessageResponse deleteDoctor(Integer doctorId);
     Doctor getASingleDoctor(Integer doctorId);
     List<Doctor> getAllDoctor();
 }
+
